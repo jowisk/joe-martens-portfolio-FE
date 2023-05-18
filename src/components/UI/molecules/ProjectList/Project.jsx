4f9444f08db90c1491c211e368b44f184b546f2f
Jowi <jnmartenss@gmail.com>
2023-05-18 16:43:40 -0300

    feat(styles): added animations on project cards w framer-motion library & updated deploy status

--- src/components/UI/molecules/ProjectList/Project.jsx
+++ src/components/UI/molecules/ProjectList/Project.jsx
@@ -1,20 +1,24 @@
 import { React, useState } from 'react'
+import { motion, transform } from 'framer-motion'
 
 const Project = ({ name, technologies, src, deploy, git }) => {
 
     const [isVisible, setIsVisible] = useState(false)
+    const [isDeployed, setIsDeployed] = useState(deploy.length != 0)
+
 
     const handleVisibility = () => {
         setIsVisible(!isVisible)
-        console.log(isVisible)
     }
 
     return (
         <div className="mt-[40px] lg:mt-0 flex flex-col w-full h-[398px] lg:w-[540px] lg:h-[487px] relative" onMouseEnter={handleVisibility} onMouseLeave={handleVisibility}>
-            {isVisible && <div className="hidden lg:flex justify-between absolute z-[3] right-[307px] top-[167px] bg-[#242424] w-[400px] rotate-90 uppercase text-[20px] p-[20px] leading-[26px] tracking-[2.29px] font-[700]">
-                <a href={deploy}><p className="hover-underline-animation hover:text-[#4EE1A0] transition duration-300 hover:ease-in">view project</p></a>
+
+            <motion.div animate={{ scaleX: isVisible ? 1 : 0 }} initial={{ scaleX: 0, rotate: 90 }} className="hidden lg:flex justify-between absolute z-[3] right-[307px] top-[167px] bg-[#242424] w-[400px] rotate-90 uppercase text-[20px] p-[20px] leading-[26px] tracking-[2.29px] font-[700]">
+                {isDeployed ? <a href={deploy}><p className="hover-underline-animation hover:text-[#4EE1A0] transition duration-300 hover:ease-in">view project</p></a> : <p className="text-[16px]">Not deployed yet :(</p>}
                 <a href={git}><p className="hover-underline-animation hover:text-[#4EE1A0] transition duration-300 hover:ease-in">view code</p></a>
-            </div>}
+            </motion.div>
+
             <img className="w-full h-full lg:w-[540px]" src={src} alt="" srcSet="" />
             <p className="uppercase mt-[20px] font-[700] text-[24px] leading-[32px]">{name}</p>
             <div className="flex mt-[7px] text-[#D9D9D9] w-full">
